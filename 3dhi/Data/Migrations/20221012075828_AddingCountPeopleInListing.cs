c8f4546d9a6a8d61af48941c49703416f1713f3b
Ivan Valkov <xcal1bur@abv.bg>
2022-10-12 10:59:28 +0300

    Migration added - editing Listing

---
+++ 3dhi/Data/Migrations/20221012075828_AddingCountPeopleInListing.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _3dhi.Migrations
+{
+    public partial class AddingCountPeopleInListing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MainPhoto",
+                table: "Listings",
+                newName: "MainPhotoPath");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Extension",
+                table: "PhotoPaths",
+                type: "nvarchar(200)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "placesAdults",
+                table: "Listings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "placesChildren",
+                table: "Listings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Extension",
+                table: "PhotoPaths");
+
+            migrationBuilder.DropColumn(
+                name: "placesAdults",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "placesChildren",
+                table: "Listings");
+
+            migrationBuilder.RenameColumn(
+                name: "MainPhotoPath",
+                table: "Listings",
+                newName: "MainPhoto");
+        }
+    }
+}
