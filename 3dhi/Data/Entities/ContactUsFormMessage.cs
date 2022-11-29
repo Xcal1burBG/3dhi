748ec8d6d7ca3014983cb7c34c2a0de59ed76c40
Ivan Valkov <xcal1bur@abv.bg>
2022-11-29 16:37:42 +0200

    ContactUs form entity created

---
+++ 3dhi/Data/Entities/ContactUsFormMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace _3dhi.Data.Entities
+{
+    public class ContactUsFormMessage
+    {
+        public string SenderName { get; set; }
+        public string Email { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Message { get; set; }
+
+#nullable enable
+        public string? PhoneNumber { get; set; }
+    }
+}
