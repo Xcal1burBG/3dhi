8dd20ee3a1aaf6e0c716b9f95382842b5ea92f58
Ivan Valkov <xcal1bur@abv.bg>
2022-12-02 22:55:55 +0200

    Àdmin adding buttons for Stay and RealEstates pages

---
+++ 3dhi/Models/ViewModels/ListingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using _3dhi.Data.Entities;
+using _3dhi.Enum;
+
+namespace _3dhi.Models.ViewModels
+{
+    public class ListingViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
+        public int placesAdults { get; set; }
+        public int placesChildren { get; set; }
+        public string MainPhotoPath { get; set; }
+        public EntityStatus EntityStatus { get; set; }
+        public virtual User User { get; set; }
+
+#nullable enable
+        public ICollection<PhotoPath>? PhotoPaths { get; set; }
+    }
+}
